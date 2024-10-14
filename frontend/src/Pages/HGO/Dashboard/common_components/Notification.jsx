506020bfaab2893a07082a619da9efafeeebafc3
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-10-14 10:48:43 +0500

    notification responsive now

--- frontend/src/Pages/HGO/Dashboard/common_components/Notification.jsx
+++ frontend/src/Pages/HGO/Dashboard/common_components/Notification.jsx
@@ -9,7 +9,7 @@ const Notification = ({ isOpen, onClose }) => {
         <div className="modal-content notification-modal-height">
           <div className="modal-header">
             <h5 className="modal-title">Notification</h5>
-            <button type="button" className="btn-close" onClick={onClose}></button>
+            <button type="button" className="btn-close btn-close-notification" onClick={onClose}></button>
           </div>
           <div className="modal-body">
           <p>This is an empty modal.</p>
